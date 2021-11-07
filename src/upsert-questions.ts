f1551b02b39ae96950ab3082071b48c1e452ed74
Zachary Roberts <zroberts@auracompany.com>
2021-11-07 09:30:57 -0500

    fix stuff

--- src/upsert-questions.ts
+++ src/upsert-questions.ts
@@ -11,7 +11,7 @@ const questions = [
     title: 'Sum of Two Numbers',
     description: `Write a function that **returns the sum of two numbers**. \n
 ## Example \n
-2 + 3 = 5"; \n
+2 + 3 = 5 \n
 `,
     initialCode: `
 function sumNums(a, b) { 
@@ -23,9 +23,14 @@ function sumNums(a, b) {
   {
     fnName: 'countDown',
     title: 'Count from 10 to 1',
-    description: `Write a loop inside a function that **returns integers descending from 10 to 1**. <br> 
-    ## Example <br> 
-    10<br>9<br>8<br>7<br>6<br>..";
+    description: `Write a loop inside a function that **returns integers descending from 10 to 1**.
+## Example 
+10\n
+9\n
+8\n
+7\n
+6\n
+...
     `,
     initialCode: `
 function countDown() {
@@ -39,26 +44,26 @@ function countDown() {
     fnName: 'totalPoints',
     title: 'FinalScores',
     description: `You are adding up points for a basketball game, **given the amount** of **2-pointers made and 3-pointers made**, find the final points for the team and **return that value**. \n
-    ## Examples \n
-    **Input** (1, 1) -> **Output** (5)
-    **Input** (5, 5) -> **Output** (25)
-    **Input** (10, 10) -> **Output** (50)
+## Examples \n
+**Input** (1, 1) -> **Output** (5) \n
+**Input** (5, 5) -> **Output** (25) \n
+**Input** (10, 10) -> **Output** (50)
     `,
     initialCode: `
 function totalPoints(twoPointers, threePointers) {
     // return something
 }`,
     week: 1,
-    day: 4,
+    day: 3,
   },
   {
     fnName: 'findWord',
     title: "What's the word?",
     description: `Oh no! I can't find the word hidden amongst the trees. Help me write a function that finds the lower case word hidden in an string of random letters. \n
-  ## Examples \n
-  **Input** findWord("ALOcURTFoYFEdTWUiIOWUTWnOUOFWg") -> **Output** ("coding")
-  **Input** findWord("GEdWKUoGETFDg") -> **Output** ("dog")
-  **Input** findWord("THhTREeUIEaHEYlHWtDh") -> **Output**("health")
+## Examples \n
+**Input** findWord("ALOcURTFoYFEdTWUiIOWUTWnOUOFWg") -> **Output** ("coding")\n
+**Input** findWord("GEdWKUoGETFDg") -> **Output** ("dog")\n
+**Input** findWord("THhTREeUIEaHEYlHWtDh") -> **Output**("health")\n
   `,
     initialCode: `
 function findWord() {
@@ -67,6 +72,30 @@ function findWord() {
   `,
     week: 2,
     day: 1,
+  },
+  {
+    fnName: 'timesTable',
+    title: 'Multiplication Table',
+    description: `
+Write a function the creates the multiplication table and outputs it in the terminal.`,
+    initialCode: `
+for (let  i = 1; i <=10; i++) {
+    // do something
+}
+`,
+    week: 2,
+    day: 2,
+  },
+  {
+    fnName: 'timesTable',
+    title: 'Multiplication Table 2',
+    description: `Write a function the creates the multiplication table and outputs it in the terminal.`,
+    initialCode: `
+for (let  i = 1; i <=10; i++) {
+    // do something
+}`,
+    week: 2,
+    day: 3,
   },
 ] as Question[];
 
@@ -87,7 +116,7 @@ const testCases = [
 
 const run = async () => {
   await Promise.all(questions.map(createQuestion));
-  await Promise.all(testCases.map(createTestCase));
+  //   await Promise.all(testCases.map(createTestCase));
   process.exit();
 };
 
